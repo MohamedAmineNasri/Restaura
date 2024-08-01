90d07ff78d74680f0e52981563ec4971d3b6392b
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-08-01 15:54:40 +0100

    Fixing Navbar Section In Mobile Screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         setIsMobileMenuOpen(false)   
     }
   return (
-    <nav className="fixed flex top-4 z-50 w-full flex-col items-center justify-center">
+    <nav className="fixed  top-4 z-50 flex w-full flex-col items-center justify-center">
         <div className="flex w-full items-center justify-between
             overflow-y-hidden p-4 backdrop-blur-lg lg:m-2 lg:w-[50rem]
             lg:rounded-full lg:shadow-lg
@@ -30,7 +30,7 @@ const Navbar = () => {
             <div className="hidden space-x-6 lg:flex">
                 {LINKS.map((link, index) => (
                     <a key={index} href={`#${link.targetId}`}
-                        className={`text-sm ${index !== 0 ? "border-l-2 border-neutral-300/20 pl-2 " : ''}hover:opacity-50`}
+                        className={`text-sm ${index !== 0 ? "border-l-2 border-neutral-300/20 pl-2 " : ''} hover:opacity-50`}
                         onClick={(e) => handleScroll(e, link.targetId)}
                     >
                         {link.text}
@@ -42,6 +42,7 @@ const Navbar = () => {
                     {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
                 </button>
             </div>
+        </div>
             {
                 isMobileMenuOpen && (
                     <div className="w-full backdrop-blur-lg lg:hidden">
@@ -53,7 +54,6 @@ const Navbar = () => {
                     </div>
                 )
             }
-        </div>
     </nav>
   )
 }
